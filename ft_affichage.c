1d681b03c4797686b983b0cf95751bfecba792dc
Theo LIOT <tliot@e1r13p10.clusters.42paris.fr>
2022-05-17 17:28:52 +0200

    ajoute image tranparent

--- ft_affichage.c
+++ ft_affichage.c
@@ -1,40 +1,40 @@
 #include "so_long.h"
 #include "minilibx-linux/mlx.h"
 
-unsigned int    get_color_pixel(t_data *all, void *img, int y, int x)
+unsigned int    get_color_pixel(t_data *imgs, void *img, int y, int x)
 {
     char    *src;
     int        color;
 
-    all->addr = mlx_get_data_addr(img, &all->bits_per_pixel,
-            &all->line_length, &all->endian);
-    src = all->addr + (y * all->line_length + x * (all->bits_per_pixel / 8));
+    imgs->addr = mlx_get_data_addr(img, &imgs->bits_per_pixel,
+            &imgs->line_length, &imgs->endian);
+    src = imgs->addr + (y * imgs->line_length + x * (imgs->bits_per_pixel / 8));
     color = *(unsigned int *)src;
     return (color);
 }
 
-void    my_mlx_pixel_put(t_data *all, int x, int y, int color)
+void    my_mlx_pixel_put(t_data *imgs, int x, int y, int color)
 {
     char    *dst;
 
-    dst = all->addr + (y * all->line_length + x * (all->bits_per_pixel / 8));
+    dst = imgs->addr + (y * imgs->line_length + x * (imgs->bits_per_pixel / 8));
     *(unsigned int *)dst = color;
 }
 
-void    put_pixel(t_data *all, void *img, int x, int y)
+void    put_pixel(t_data *imgs, void *img, int x, int y)
 {
     int    i;
     int    j;
 
     i = -1;
-    while (++i < all->img_height)
+    while (++i < imgs->img_height)
     {
         j = -1;
-        while (++j < all->img_width)
+        while (++j < imgs->img_width)
         {
-            all->color = get_color_pixel(all, img, i, j);
-            if (!(all->color == (0xFF << 24)))
-                my_mlx_pixel_put(all, x + j, y + i, all->color);
+            imgs->color = get_color_pixel(imgs, img, i, j);
+            if (!(imgs->color == (0xFF << 24)))
+                my_mlx_pixel_put(imgs, x + j, y + i, imgs->color);
         }
     }
 }
@@ -47,20 +47,20 @@ t_data	*ft_return_herbe_img(t_data *imgs,char **tab, int x, int y)
 	
 
 	if(tab[y][x+1] && tab[y][x+1] == '0')
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbeD.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbeD.xpm"));
 	if(x > 0 && tab[y][x-1] == '0')
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbeG.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbeG.xpm"));
 	i++;
 	if(i == 1)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbe1.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbe1.xpm"));
 	if(i == 2)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbe2.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbe2.xpm"));
 	if(i == 3)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbe3.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbe3.xpm"));
 	if(i == 4)
 	{
 		i = 0;
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/herbe4.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/herbe4.xpm"));
 	}
 	
 
@@ -73,20 +73,20 @@ t_data	*ft_return_terre_img(t_data *imgs,char **tab, int x, int y)
 	
 
 	if(x != 0 && tab[y][x+1] && tab[y][x+1] != '1' )
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terreD.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terreD.xpm"));
 	else if (x == 0 || (x > 0 && tab[y][x-1] != '1'))
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terreG.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terreG.xpm"));
 	i++;
 	if(i == 1)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terre1.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terre1.xpm"));
 	if(i == 2)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terre2.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terre2.xpm"));
 	if(i == 3)
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terre3.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terre3.xpm"));
 	if(i == 4)
 	{
 		i = 0;
-		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/terre4.xpm"));
+		return(ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/terre4.xpm"));
 	}
 	
 
@@ -127,7 +127,7 @@ void	ft_put_background_img(t_map *data, t_data *imgs, void	*mlx,void	*mlx_win)
 	int x;
 
 	x = 0;
-	img_put = ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/fond506.xpm");
+	img_put = ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/fond506.xpm");
 	while(x < data->size_x*32)
 	{
 		mlx_put_image_to_window(mlx, mlx_win, img_put, x, (data->size_y*32)-img_put->img_height-32);
@@ -135,7 +135,7 @@ void	ft_put_background_img(t_map *data, t_data *imgs, void	*mlx,void	*mlx_win)
 	}
 	x = 0;
 	y = (data->size_y*32)-(img_put->img_height*2);
-	img_put = ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/fond506H.xpm");
+	img_put = ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/fond506H.xpm");
 	while(y > 0)
 	{
 		while(x < data->size_x*32)
@@ -175,22 +175,22 @@ void ft_affichage(char **tab, t_map *data)
 	new.mlx_win = mlx_win;
 	/////////////////
 
-	imgs = ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/fond506.xpm");
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/fond506H.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbe1.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbe2.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbe3.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbe4.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbeD.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/herbeG.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terre1.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terre2.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terre3.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terre4.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terreD.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/terreG.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/blockP1.xpm"));
-	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX/block/blockB.xpm"));
+	imgs = ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/fond506.xpm");
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/fond506H.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbe1.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbe2.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbe3.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbe4.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbeD.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/herbeG.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terre1.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terre2.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terre3.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terre4.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terreD.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/terreG.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/blockP1.xpm"));
+	ft_lstadd_back(imgs, ft_lstnew(mlx, mlx_win , "sprite/Sheet-XPM-32PX-transparency/block/blockB.xpm"));
 
 	// A FINIR //
 	ft_put_background_img(data, imgs, mlx,mlx_win);
@@ -213,9 +213,9 @@ void ft_affichage(char **tab, t_map *data)
 					mlx_put_image_to_window(mlx, mlx_win, ft_return_herbe_img(imgs,tab,x,y), x*32, y*32);
 			}
 			if(tab[y][x] == '2')
-				mlx_put_image_to_window(mlx, mlx_win, ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/blockP1.xpm"), x*32, y*32);
+				mlx_put_image_to_window(mlx, mlx_win, ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/blockP1.xpm"), x*32, y*32);
 			if(tab[y][x] == '3')
-				mlx_put_image_to_window(mlx, mlx_win, ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX/block/blockB.xpm"), x*32, y*32);	
+				mlx_put_image_to_window(mlx, mlx_win, ft_lst_cache_cache(imgs, "sprite/Sheet-XPM-32PX-transparency/block/blockB.xpm"), x*32, y*32);	
 			//if(tab[y][x] == 'P')
 				//mlx_put_image_to_window(mlx, mlx_win, img3, x*32, y*32-64);
 			x++;
